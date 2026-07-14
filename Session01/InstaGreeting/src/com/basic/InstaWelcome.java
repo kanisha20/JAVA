f16e9ddcc9db447fdbe55f7256fd048437e0c31b
kanisha20 <bhelosekanisha20@gmail.com>
2026-07-14 22:37:51 +0530

    java
    session01

---
+++ Session01/InstaGreeting/src/com/basic/InstaWelcome.java
@@ -0,0 +1,19 @@
+// JDK : java development kit. used to develop java program
+// JRE : java runtime environment. used to run java program.
+// JVM : java virtual machine. used to execute java program.
+// JDK include JRE. JRE include JVM.
+package com.basic;
+
+// class declaration
+public class InstaWelcome 
+{
+	// main method
+	public static void main(String[] args) 
+	{
+		//print statement
+		System.out.println("Welcome to Instagram Java Edition!");
+		System.out.println("Instragram...");
+		System.out.println("Snapchat...");
+		System.out.println("Whatsapp..");
+	}
+}
